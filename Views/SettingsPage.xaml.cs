197353be58e0d0ce9a3f9decf15035f5e3b975ed
Zhiwen <48937482+cat8806@users.noreply.github.com>
2025-06-30 13:04:03 -0700

    Remove placeholder tab icons

---
+++ Views/SettingsPage.xaml.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.Maui.Controls;
+namespace MigraineTracker.Views;
+
+public partial class SettingsPage : ContentPage
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
