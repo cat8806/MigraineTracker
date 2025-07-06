5682161500fb32f10bc9995da95d09f43db36e22
Zhiwen <48937482+cat8806@users.noreply.github.com>
2025-07-06 15:33:23 -0700

    Disable splash screen

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace MigraineTracker
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.MainTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
